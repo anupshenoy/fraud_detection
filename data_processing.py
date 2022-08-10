1800d4e1b8b067f58dacdeb812281f7d40dd5df8
anupshenoy <anupshenoy.iiitb@gmail.com>
2022-08-10 22:35:25 +0530

    Revert "corrected the error in return param in sum function"
    This reverts commit 11b3a1aa46545e6a01fb816a1a92e9e1d9ffaff1.

--- data_processing.py
+++ data_processing.py
@@ -6,9 +6,7 @@ print('Adding all the data processing code related to Fraud detection')
 ## add a new function
 
 def sum(a,b):
-    a=5
-    b=2
     c = a+b
-    return c
+    return a
 
-print('added a new function sum which is NOW correct')
+print('added a new function sum which is not correct')
