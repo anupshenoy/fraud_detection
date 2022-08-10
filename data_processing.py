b13cf2bce0bb4d6ab999f2048392fdd3fd74257b
anupshenoy <anupshenoy.iiitb@gmail.com>
2022-08-10 22:29:09 +0530

    added new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,12 @@
+from re import A
+
+
 print('Adding all the data processing code related to Fraud detection')
+
+## add a new function
+
+def sum(a,b):
+    c = a+b
+    return a
+
+print('added a new function sum which is not correct')
