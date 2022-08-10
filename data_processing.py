11b3a1aa46545e6a01fb816a1a92e9e1d9ffaff1
anupshenoy <anupshenoy.iiitb@gmail.com>
2022-08-10 22:34:46 +0530

    corrected the error in return param in sum function

--- data_processing.py
+++ data_processing.py
@@ -6,7 +6,9 @@ print('Adding all the data processing code related to Fraud detection')
 ## add a new function
 
 def sum(a,b):
+    a=5
+    b=2
     c = a+b
-    return a
+    return c
 
-print('added a new function sum which is not correct')
+print('added a new function sum which is NOW correct')
